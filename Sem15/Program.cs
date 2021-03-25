f16a371b8ee28382774893210f134e55bac66aad
Gorbunov-PI18-3 <stfouley5@gmail.com>
2021-03-25 12:26:28 +0300

    Добавьте файлы проекта.

---
+++ Sem15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sem15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var ClassroomByStud = from Classroom in classes
+                                  group Classroom by Classroom.Students;
+            //foreach (var Classroom in ClassroomByStud)
+                return ClassroomByStud;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
